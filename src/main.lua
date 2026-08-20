99a13ffbadc266f0e25f7ac3ac51108457511621
Antonio <64821480+MINTILER-DEV@users.noreply.github.com>
2026-08-20 18:23:01 -0500

    Update main.lua

--- src/main.lua
+++ src/main.lua
@@ -1,1 +1,7 @@
-
+local testmodule = {}
+testmodule.hello = function(name: string)
+  print("Hello, " .. name .. "!")
+  print("package_dir is " .. PACKAGE_DIR)
+  print("source_dir is " .. SOURCE_DIR)
+end
+return testmodule
